78d4eafc3bf62783aaa0aa55359ba0998bef0c01
DESKTOP-A8CUS40\malyo <valentin.maliovanik@gmail.com>
2023-04-30 22:23:18 +0300

    #1: Create base UI for choosing translation of words

---
+++ View/LearnAndGrowView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace LearnAndGrow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LearnAndGrowView : Window
+    {
+        public LearnAndGrowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Word.cs
@@ -0,0 +1,16 @@
+﻿namespace LearnAndGrow
+{
+    public class Word
+    {
+        public Word(int numberInVocabulary, string wordInEnglish, string wordInUkrainian)
+        {
+            NumberInVocabulary = numberInVocabulary;
+            WordInEnglish = wordInEnglish;
+            WordInUkrainian = wordInUkrainian;
+        }
+
+        public readonly int NumberInVocabulary;
+        public readonly string WordInEnglish;
+        public readonly string WordInUkrainian;
+    }
+}
