1bd63329028e8dd2e87c2ea9934539edc7122033
DESKTOP-A8CUS40\malyo <valentin.maliovanik@gmail.com>
2023-05-02 22:28:31 +0300

    #3: Created simple start view

--- ViewModel/Childs/AddNewWordViewModel.cs
+++ ViewModel/Childs/AddNewWordViewModel.cs
@@ -1,13 +1,35 @@
-﻿using LearnAndGrow.Utilities;
+﻿using LearnAndGrow.Commands;
+using LearnAndGrow.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LearnAndGrow.ViewModel.Childs
 {
     internal class AddNewWordViewModel : BaseViewModel
     {
+
+        public AddNewWordViewModel() { }
+        private ICommand _addWordInVocabularyCommand;
+
+        public ICommand AddWordInVocabularyCommand
+        {
+            get
+            {
+                if (_addWordInVocabularyCommand == null)
+                {
+                    _addWordInVocabularyCommand = new RelayCommand(param => AddWordCommand());
+                }
+                return _addWordInVocabularyCommand;
+            }
+        }
+
+        private void AddWordCommand()
+        {
+
+        }
     }
 }
